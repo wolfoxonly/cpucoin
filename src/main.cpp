c1d13db734cac80fe0728ebd23245d2a09115d37
Icbitproject <16358164@qq.com>
2018-08-27 16:41:17 +0800

    merge

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Bitcoin Signed Message:\n";
+const string strMessageMagic = "Cpucoin V1.0 Signed Message:\n";//coingo
 
 // Internal stuff
 namespace {
